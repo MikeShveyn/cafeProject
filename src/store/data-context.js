e4d7e2148382a6bdc7d2cfa60054b11b93ffbd35
Michael <50995165+MikeShveyn@users.noreply.github.com>
2021-12-12 14:52:05 +0200

    Setup firebase and mui

--- src/store/data-context.js
+++ src/store/data-context.js
@@ -2,6 +2,7 @@ import {createContext, useState} from 'react';
 
 const DataContext = createContext({
    data : [],
+   firebaseConfig,
    addData : (newData) => {},
    removeData: (id) => {}
 });
@@ -11,6 +12,15 @@ export function DataContextProvider(props) {
 
     const context = {
         data : state,
+        firebaseConfig : {
+            apiKey: "AIzaSyA0-hEc_79Do9rcDUUFa4TklGJui3jbAWI",
+            authDomain: "cafe-f0195.firebaseapp.com",
+            databaseURL: "https://cafe-f0195-default-rtdb.firebaseio.com",
+            projectId: "cafe-f0195",
+            storageBucket: "cafe-f0195.appspot.com",
+            messagingSenderId: "323160095814",
+            appId: "1:323160095814:web:18b3004470a140feddde47"
+          },
         addData : addDataHandler,
         removeData: removeDataHandler
     };
