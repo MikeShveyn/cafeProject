3e5fe96d10a9fb679d4b95a17caa368f7566a7d6
Michael <50995165+MikeShveyn@users.noreply.github.com>
2021-12-12 19:46:08 +0200

    design color chaneges

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,9 +1,22 @@
+import css from "./Main.module.css";
+
 function Main() {
-    return (
-        <div>
-            Main
+  return (
+    <div>
+      <header>
+        <div className={css.title}>Cofe Shop</div>
+        <div className={css.headerImg}>
+          <img src="assets/imgs/main-page-header.jpg" alt="header" />
         </div>
-    )
+      </header>
+      <main>
+        
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  );
 }
 
 export default Main;
