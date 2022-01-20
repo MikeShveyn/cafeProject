416610b19c4663c0918cf93b0c5d26f051d123a6
Michael <50995165+MikeShveyn@users.noreply.github.com>
2022-01-21 00:44:05 +0200

    dialog

--- src/components/menuItem/MenuItem.js
+++ src/components/menuItem/MenuItem.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 import css from "./MenuItem.module.css";
 import Card from "../ui/Card/Card";
+import { Button } from "../ui/Button/Button";
+import CafeDialog from "../ui/Dialog/CafeDialog";
 
 function MenuItem(props) {
+
+
+
   return (
     <li className={css.container}>
       <Card>
+       <Button label={"Edit"} action={() => {CafeDialog}} /> 
         <div className={css.flex}>
           <img src="assets/imgs/item.png" alt="image" />
           <p className={css.title}>{props.title}</p>
           <p className={css.descr}>{props.description}</p>
-          <p className={css.price}>{'Price: ' +  props.price}</p>
+          <p className={css.price}>{"Price: " + props.price}</p>
         </div>
       </Card>
     </li>
