17b3db1d15e448fdd21fe010d7b4ce1cec7b6b6a
Anna Shveynfurt <57673883+AnnaShw@users.noreply.github.com>
2022-01-21 17:44:20 +0200

    First one done

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -2,14 +2,14 @@ import React from "react";
 import Button from '../../components/ui/Button/Button';
 import classes from './Order.module.css';
 
-
 function Order() {
     return(
-            <div className={classes.mainD}>
+            <div className={classes.mainD} >
                 please, check where do you want to seat:
                 <div className={classes.optionDiv}>
                     <Button label={'Inside'}/>
                     <Button label={'Outside'}/>
+                    <Button label={'Take away'}/>
                 </div>
             </div>
     )
