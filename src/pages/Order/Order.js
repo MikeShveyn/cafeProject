c0d6b3f7e67ed0c0c44337855a077d137754a7b0
Anna Shveynfurt <57673883+AnnaShw@users.noreply.github.com>
2022-01-21 17:27:26 +0200

    Inside/Outside

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Button from '../../components/ui/Button/Button';
+import classes from './Order.module.css';
+
 
 function Order() {
     return(
-        <div>
-            Order
-        </div>
+            <div className={classes.mainD}>
+                please, check where do you want to seat:
+                <div className={classes.optionDiv}>
+                    <Button label={'Inside'}/>
+                    <Button label={'Outside'}/>
+                </div>
+            </div>
     )
 }
 
